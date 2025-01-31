f6860313f9d73346b319c3b9740e65489ac01b3d
flopears <theflopear@gmail.com>
2025-01-31 14:20:26 -0500

    Updated items with neoforge functions

---
+++ src/main/java/net/flopear/flopsblocks/block/ModBlocks.java
@@ -0,0 +1,8 @@
+package net.flopear.flopsblocks.block;
+
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks("flopsblocks");
+}
