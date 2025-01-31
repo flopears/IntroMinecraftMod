40dc184c3529d0234107ef6efa4094241e0b9bd0
flopears <theflopear@gmail.com>
2025-01-31 15:16:23 -0500

    added first block registry

--- src/main/java/net/flopear/flopsblocks/block/ModBlocks.java
+++ src/main/java/net/flopear/flopsblocks/block/ModBlocks.java
@@ -1,8 +1,28 @@
 package net.flopear.flopsblocks.block;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks("flopsblocks");
+
+    public static final DeferredBlock<Block> FLOPS_FIRST_BLOCK = BLOCKS.register(
+            "flops_first_block",
+            registryName -> new Block(BlockBehaviour.Properties.of()
+                    .setId(ResourceKey.create(Registries.BLOCK, registryName))
+                    .destroyTime(2.0f)
+                    .explosionResistance(10.0f)
+                    .sound(SoundType.AMETHYST)
+                    .lightLevel(state -> 10)
+            ));
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
