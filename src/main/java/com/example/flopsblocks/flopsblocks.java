a911f0f033b287d2dd0152663390dc6ad0b1525d
flopears <theflopear@gmail.com>
2025-01-31 13:33:55 -0500

    Fixed Capitalization issues

--- src/main/java/com/example/flopsblocks/flopsblocks.java
+++ src/main/java/com/example/flopsblocks/flopsblocks.java
@@ -35,11 +35,11 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(FlopsBlocks.MOD_ID)
-public class FlopsBlocks
+@Mod(flopsblocks.MOD_ID)
+public class flopsblocks
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "FlopsBlocks";
+    public static final String MOD_ID = "flopsblocks";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -69,7 +69,7 @@ public class FlopsBlocks
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public FlopsBlocks(IEventBus modEventBus, ModContainer modContainer)
+    public flopsblocks(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
