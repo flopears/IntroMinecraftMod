377ea43e51bcbd74c1300ab5410593bcbce8d1ca
MeganHulsey <cyclonekitty@gmail.com>
2025-02-03 13:27:19 -0500

    Creating directories (models, item, textures) and updating addListener for addCreative in flopsblocks.java

--- src/main/java/net/flopear/flopsblocks/flopsblocks.java
+++ src/main/java/net/flopear/flopsblocks/flopsblocks.java
@@ -74,7 +74,7 @@ public class flopsblocks
     public flopsblocks(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
 
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
@@ -112,8 +112,8 @@ public class flopsblocks
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+            event.accept(ModItems.Sapphire);
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
